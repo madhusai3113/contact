1d799179f22b6020e89f35a949ce8de05c9b8491
madhusai3113 <madhusairavada@gmail.com>
2015-07-10 12:30:59 +0530

    Delete update.php

--- update.php
+++
@@ -1,8 +0,0 @@
-
-
-
-<?php 
-	include 'config.php';
-	session_start();
-		    $query = mysqli_query($connection,"UPDATE users SET `username` = $_POST['username'] , `phone` = $_POST['phone'] WHERE `username` = $_SESSION['username']);
-?>
